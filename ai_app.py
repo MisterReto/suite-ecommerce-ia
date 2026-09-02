29b8124ff086a8f05c78e471e0378414151c1b88
MisterReto <aretana150405@gmail.com>
2026-09-01 23:15:48 -0600

    chore: renovar caché de navegación y tutorial

--- ai_app.py
+++ ai_app.py
@@ -109,8 +109,8 @@ old_head = """TUTORIAL_HEAD = \"\"\"
 new_head = """TUTORIAL_HEAD = \"\"\"
 <link rel=\"stylesheet\" href=\"/static/tutorial.css?v=2\">
 <link rel=\"stylesheet\" href=\"/static/ui.css?v=1\">
-<script defer src=\"/static/tutorial.js?v=2\"></script>
-<script defer src=\"/static/accessibility.js?v=1\"></script>
+<script defer src=\"/static/tutorial.js?v=3\"></script>
+<script defer src=\"/static/accessibility.js?v=2\"></script>
 \"\"\""""
 _replace_once(old_head, new_head, "assets de interfaz accesible")
 
